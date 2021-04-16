3246941bca830cf3723e02e9e64ac6f2db7905b4
fatih meral <fatihmer0029@gmail.com>
2021-04-16 23:26:39 +0300

    site diye elemt oluşturuldu site tanımlandi

--- ntpvizeodev_FM/Program.cs
+++ ntpvizeodev_FM/Program.cs
@@ -3,19 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace ntpvizeodev_FM
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
-            int a;
+            XElement site;
+            site = XElement.Load("http://rss.beyazperde.com/haberler/filmler?format=xml");
+
+            
+            Console.ReadKey();
         }
 
-        public static void yeni()
-        {
-
-        }
+       
     }
 }
