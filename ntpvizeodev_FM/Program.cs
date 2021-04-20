5f526845f25b93fff4aa2532cea7442c12c9a58e
fatih meral <fatihmer0029@gmail.com>
2021-04-20 15:47:14 +0300

    timer kutuphanesinii kullandık zamansal olarak geçikme yani veriyi yenileye bilmek için while true döngüsü main metodunun bitip işlemi sonladrımasın diye eklenmiştir

--- ntpvizeodev_FM/Program.cs
+++ ntpvizeodev_FM/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
 using System.IO;
+using System.Timers;
 
 namespace ntpvizeodev_FM
 {
@@ -13,11 +14,21 @@ namespace ntpvizeodev_FM
         
         static void Main(string[] args)
         {
+            Timer t = new Timer(2000);
+            t.Elapsed += new ElapsedEventHandler(islemler);
+            t.Start();
+            while (true)
+            {
 
+            }
             
+        }
+
+        public static void islemler(object o, ElapsedEventArgs a)
+        {
             if (File.Exists("deneme.txt"))
             {
-                
+
             }
             else
             {
@@ -25,8 +36,8 @@ namespace ntpvizeodev_FM
                 Console.WriteLine("eklendi");
                 fs.Close();
             }
-            
-            
+
+
             XElement site;
             site = XElement.Load("http://rss.beyazperde.com/haberler/filmler?format=xml");
 
@@ -35,21 +46,14 @@ namespace ntpvizeodev_FM
 
             StreamWriter Yaz = new StreamWriter("deneme.txt", true);
             int uzunluk;
-
-
             
             foreach (var item in list)
             {
-                
-                
-
                 Console.WriteLine(item.Element("title").Value);
                 Console.WriteLine(Convert.ToDateTime(item.Element("pubDate").Value));////<----///
                 XElement aciklama = item.Element("description");
                 uzunluk = aciklama.Value.IndexOf("&", 0, aciklama.Value.Length - 1);
-                Console.WriteLine(aciklama.Value.Substring(0, uzunluk-3));
-
-
+                Console.WriteLine(aciklama.Value.Substring(0, uzunluk - 3));
                 Console.WriteLine("\n-------------------------------------------\n");
 
                 Yaz.WriteLine(item.Element("title").Value);
@@ -58,12 +62,11 @@ namespace ntpvizeodev_FM
                 Yaz.WriteLine(aciklama.Value.Substring(0, uzunluk - 3));
                 Yaz.WriteLine("\n-------------------------------------------\n");
 
-                Yaz.Close();
             }
-            
+            Yaz.Close();
+
             Console.ReadKey();
         }
-
        
     }
 }
